fce9ae166d995e1176e67069bfcb99a58a309ad6
AllanTig <allantig748@gmail.com>
2023-02-22 19:36:46 -0300

    1º Commit

---
+++ app/src/main/java/com/example/precificadordeprodutos/ui/Precificador/PrecificadorFragment.java
@@ -0,0 +1,14 @@
+package com.example.precificadordeprodutos.ui.Precificador;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.arch.lifecycle.ViewModelProvider;
+
+public class PrecificadorFragment extends Fragment {
+
+}
